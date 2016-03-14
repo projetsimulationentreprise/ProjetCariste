a3fdd0bdaf0fc1fadeb2ef45de43b255c5795629
David Huyn <davidhuynlive@hotmail.fr>
2016-03-14 10:48:37 +0100

    Update users.js

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var users = require('../controllers/Users'); // Nous allons récuperer notre controlleur fait précédement
+
+/* GET Récupère la liste des utilisateurs */
+router.get('/', users.index);
+
+/* POST Création d'un nouvel utilisateur */
+router.post('/', users.create);
+
+/* PUT Modification d'un utilisateur */
+router.put('/:id(\\d+)', users.update);
+
+/* DELETE Suppression d'un utilisateur */
+router.delete('/:id(\\d+)', users.delete);
 
 module.exports = router;
