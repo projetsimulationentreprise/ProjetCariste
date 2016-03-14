cd6cf1bb4d1465297877f7df46a94d202b4220af
Guillaume Arsac <guillaume.arsac@ynov.com>
2016-03-14 16:27:12 +0100

    Create index.js

---
+++ front/javascript/index.js
@@ -0,0 +1,31 @@
+var a;
+a=1;
+
+$('.cache').fadeOut(0);
+
+$('.pop').click(function () {
+    if(a==1){
+        $('.cache').fadeIn(1);
+        a=0;
+    }
+    else{
+        $('.cache').fadeOut(1);
+        a=1;
+    }
+ });
+
+var b;
+b=1;
+
+$('.cacher').fadeOut(0);
+
+$('.pope').click(function () {
+    if(b==1){
+        $('.cacher').fadeIn(1);
+        b=0;
+    }
+    else{
+        $('.cacher').fadeOut(1);
+        b=1;
+    }
+ });
