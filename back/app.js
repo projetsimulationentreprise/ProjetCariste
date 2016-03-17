606f14bdb2ef285cd49d3426d09eb19080960f7a
David Huyn <davidhuynlive@hotmail.fr>
2016-03-17 23:25:25 +0100

    Update app.js

--- back/app.js
+++ back/app.js
@@ -1,31 +1,51 @@
 var express = require('express');
+var session  = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
 
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var Users = require('./models/users');
+var Users   = require('./models/users');
 
 var app = express();
 
-global.config = require(path.join(__dirname, "package.json")).config;
+require('./config/passport')(passport);
 
-var server = app.listen(7494, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.use(session({
+    secret: 'team7simulationentreprise',
+    resave: true,
+    saveUninitialized: true
+} ));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport);
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('port', process.env.PORT || 7494);
-// uncomment after placing your favicon in /public
+
+ //uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -70,68 +90,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.post('/signup/ok', function(req,res){
-  var mail = req.body.mail;
-  var pass = req.body.password;
-  res.send(mail + " : " + pass );
-});
 
-//app.get('/setup', function(req, res) {
-//
-//    // create a sample user
-//    var nick = new Users({
-//        name: 'Nick Cerminara',
-//        prenom: 'password'
-//    });
-//
-//    // save the sample user
-//    nick.save(function(err) {
-//        if (err) throw err;
-//
-//        console.log('User saved successfully');
-//        res.json({ success: true });
-//    });
-//});
-
-var Sequelize = require('sequelize')
-    , sequelize = new Sequelize('database', 'root', 'root', {
-      dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
-      port:    7494 // or 5432 (for postgres)
-    });
-
-var Users = sequelize.define('Users', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  nom: Sequelize.STRING,
-  mail: Sequelize.STRING,
-  password: Sequelize.STRING,
-  localisation: Sequelize.STRING,
-  skill: Sequelize.STRING,
-  move: Sequelize.STRING
-});
-
-
-sequelize
-    .sync()
-    .then(function(err) {
-      console.log('Connection has been established successfully.');
-    }, function (err) {
-      console.log('Unable to connect to the database:', err);
-    }, function () {
-      return Users.create({
-        id: '',
-        nom: '',
-        mail: '',
-        password: '',
-        localisation: '',
-        skill: '',
-        move: ''
-
-      });
-    });
 
 
 module.exports = app;
