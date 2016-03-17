3d5f8a2719df6f751691d1f296080c6a522ed2a2
David Huyn <davidhuynlive@hotmail.fr>
2016-03-17 23:53:58 +0100

    Create database.js

---
+++ back/config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'root',
+        'port' : '3301'
+    },
+	'database': 'erecrue_dev',
+    'users_table': 'Users'
+};
